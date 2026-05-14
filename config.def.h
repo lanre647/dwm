e10691361b9a97eb5fadfe8df745f902b6957db9
williams-olanrewaju <owolabilanre2006@gmail.com>
2026-05-14 18:05:17 +0100

    patch: successfully added underline patch

--- config.def.h
+++ config.def.h
@@ -51,6 +51,11 @@ static const unsigned int alphas[][3] = {
 static const char *tags[] = {"一", "二", "三", "四", "五",
                              "六", "七", "八", "九"};
 
+static const unsigned int ulinepad		= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall				= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
 static const Rule rules[] = {
     /* xprop(1):
      *   WM_CLASS(STRING) = instance, class
